159964d3f71329e7c699c554a3ed8f9b08009cee
MassStash <MassStash@gmail.com>
2015-12-16 22:49:21 -0600

    Revert "ARM/ARM64: arch_timer: add macros for bits in control register"
    This reverts commit ecef7aa1709978c225c810d164782d729b7394a9.

--- arch/arm/include/asm/arch_timer.h
+++ arch/arm/include/asm/arch_timer.h
@@ -67,16 +67,11 @@ static inline void __cpuinit arch_counter_set_user_access(void)
 
 	asm volatile("mrc p15, 0, %0, c14, c1, 0" : "=r" (cntkctl));
 
-	/* Disable user access to the timers and the physical counter */
-	/* Also disable virtual event stream */
-	cntkctl &= ~(ARCH_TIMER_USR_PT_ACCESS_EN
-			| ARCH_TIMER_USR_VT_ACCESS_EN
-			| ARCH_TIMER_VIRT_EVT_EN
-			| ARCH_TIMER_USR_PCT_ACCESS_EN);
-
+	/* disable user access to everything */
+	cntkctl &= ~((3 << 8) | (7 << 0));
 
 	/* Enable user access to the virtual counter */
-	cntkctl |= ARCH_TIMER_USR_VCT_ACCESS_EN;
+	cntkctl |= (1 << 1);
 
 	asm volatile("mcr p15, 0, %0, c14, c1, 0" : : "r" (cntkctl));
 }
